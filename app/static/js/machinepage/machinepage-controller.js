56e69e0c2e060d27abf34041865d3f06b8670184
Vik27 <vikki.vnit@gmail.com>
2018-04-03 14:25:29 +0530

    commit

--- app/static/js/machinepage/machinepage-controller.js
+++ app/static/js/machinepage/machinepage-controller.js
@@ -66,4 +66,48 @@ angular.module('fractalApp')
             ]
         });
 
+
+
+        Highcharts.chart('container2', {
+			    chart: {
+			        plotBackgroundColor: null,
+			        plotBorderWidth: null,
+			        plotShadow: false,
+			        type: 'pie'
+			    },
+			    title: {
+			        text: 'Availability Pie Chart'
+			    },
+			    tooltip: {
+			        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+			    },
+			    plotOptions: {
+			        pie: {
+			            allowPointSelect: true,
+			            cursor: 'pointer',
+			            dataLabels: {
+			                enabled: true,
+			                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+			                style: {
+			                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+			                }
+			            }
+			        }
+			    },
+			    series: [{
+			        name: 'Brands',
+			        colorByPoint: true,
+			        data: [{
+			            name: 'Availability',
+			            y: 60,
+			        }, {
+			            name: 'Unplanned Downtime',
+			            y: 30
+			        }, {
+			            name: 'Planned Downtime',
+			            y: 10
+			        }]
+			    }]
+			});
+
 }]);
